0f958b1678f0c8a215412c4249f6c6f5811cc03a
Raven <ravend@163.com>
2022-10-20 11:32:55 +0800

    update ALGRITHM

--- evo.py
+++ evo.py
@@ -1,15 +1,16 @@
 import numpy as np
-import numpy.random as random
 import time
 import copy
 import os
 
-from rlog import _log_warning, _log_info, _log_normal, _log_bg_blue, _log_bg_pp, _log_error
+from rlog import _log_warning, _log_info, _log_normal, _log_error
 
 from hparams import params as H
 
-def create_weights(shape):
-    return random.normal(0.0, np.e, shape)
+import mnist
+
+def create_weights(shape, mean=0.0, var=1.0):
+    return random.normal(mean, var, shape)
 
 def tanh(x):
     return np.tanh(x)
@@ -17,6 +18,14 @@ def tanh(x):
 def softmax(x):
     exps = np.exp(x)
     return exps / np.sum(exps, axis=-1).reshape([x.shape[0], 1])
+
+def simple_softmax(x):
+    x = x - np.min(x, axis=-1).reshape(x.shape[0], 1)
+    return x / np.sum(x, axis=-1).reshape([x.shape[0], 1])
+
+
+test_data = mnist.test_data
+test_label = mnist.test_label
 
 class Creature(object):
 
@@ -26,9 +35,6 @@ class Creature(object):
         self.data_in_dimention = hp['data_in_dimention']
         self.data_out_dimention = hp['data_out_dimention']
         self.erate = hp['erate']
-        self.FIX_ERATE = hp['erate']
-        self.decay_step = hp['decay_step']
-        self.decay_rate = hp['decay_rate']
         self.cpu_sleep_step = hp['cpu_sleep_step']
         self.cpu_sleep_time = hp['cpu_sleep_time']
         self.restore_dir = restore_dir
@@ -54,7 +60,7 @@ class Creature(object):
 
     def __build__model__(self, matrix):
         if (None != matrix):
-            _log_bg_blue('restore model from stored model file...')
+            _log_info('restore model from stored model file...')
             self.layers.append(matrix[0])
             if (self.layer_count > 2):
                 for i in range(self.layer_count - 2):
@@ -63,9 +69,8 @@ class Creature(object):
             self.layers.append(matrix[-3])
             self.bc_ce = matrix[-2]
             self.step = matrix[-1]
-            self.erate = self.FIX_ERATE * (self.decay_rate ** int(self.step / self.decay_step))
         else:
-            _log_bg_blue('create new model with random weights...')
+            _log_info('create new model with random weights...')
             self.layers = []
             self.layers.append(create_weights([self.data_in_dimention, self.neuron_count]))
             if (self.layer_count > 2):
@@ -83,24 +88,17 @@ class Creature(object):
         sdata.append(self.step)
         sdata = np.array(sdata)
         np.save(os.path.join(self.restore_dir, self.model_fn), sdata)
-        _log_bg_blue('model has been stored, ' + os.path.join(self.restore_dir, self.model_fn))
-        
-
-    def __calc_erate__(self):
-        if (self.step > 0):
-            if (self.step % self.decay_step == 0):
-                times = int(self.step / self.decay_step)
-                self.erate = self.FIX_ERATE * (self.decay_rate ** times)
+        _log_info('model has been stored, ' + os.path.join(self.restore_dir, self.model_fn))
 
     def __sleep_cpu__(self):
         if (self.step > 0):
             if (self.step % self.cpu_sleep_step == 0):
-                _log_info('CPU sleep for ' + str(self.cpu_sleep_time) + 's...')
+                _log_normal('CPU sleep for ' + str(self.cpu_sleep_time) + 's...')
                 time.sleep(self.cpu_sleep_time)
 
     def mutate(self):
         self.bc_layers = copy.deepcopy(self.layers)
-        self.__calc_erate__()
+        accu_mutate_count = 0
         for i in range(self.layer_count):
             layer = self.layers[i]
             count = layer.shape[0] * layer.shape[1]
@@ -110,10 +108,12 @@ class Creature(object):
             for j in range(mcount):
                 position = (random.randint(0, layer.shape[0], []), random.randint(0, layer.shape[1], []))
                 layer[position[0], position[1]] = create_weights([])
+            accu_mutate_count += mcount
             self.layers[i] = layer
+        _log_info('%d neurons had been mutated.' % accu_mutate_count)
         self.step += 1
         self.__sleep_cpu__()
-                
+
     def recovery(self):
         self.layers = self.bc_layers
 
@@ -121,62 +121,39 @@ class Creature(object):
         idata = data
         for i in range(self.layer_count):
             idata = tanh(np.dot(idata, self.layers[i]))
-        return idata        
+        return idata
 
-    def evolve(self, data, target):
+    def evolve(self, data, target, batch=500):
         assert data.shape[1] == self.layers[0].shape[0]
         assert target.shape[1] == self.layers[-1].shape[1]
 
         self.mutate()
-        idata = self.forward(data)
 
-        assert idata.shape[1] == target.shape[1]
+        start = 0
+        ces = []
+        while (start < len(data)):
+            end = start + batch
+            if (end > len(data)):
+                end = len(data)
+            cdata = data[start:end]
+            logits = self.forward(cdata)
+            assert logits.shape[1] == target.shape[1]
+            probs = softmax(logits)
+            ces.append(np.mean(-np.sum(target * np.log(probs), axis=-1)))
 
-        idata = softmax(idata)
-        self.ce = np.mean(-np.sum(target * np.log(idata), axis=-1))
-
+        self.ce = np.mean(ces)
         if (self.ce >= self.bc_ce):
             self.recovery()
         else:
             self.bc_ce = self.ce
-        _log_normal('BCE:%.10f (CE:%f) ERATE:%.4f, STEP:%d' % (self.bc_ce, self.ce, self.erate, self.step))
+        _log_normal('bce:%.3f (ce:%.3f) erate:%.4f, step:%d' % (self.bc_ce, self.ce, self.erate, self.step))
         return self.step
-
-    def infer(self, data):
-        idata = self.forward(data)
-        idata = softmax(idata)
-        y = np.argmax(idata, -1)
-        return y
-
-import mnist
-
-def infer(creature):
-    tcount = mnist.get_infer_batch_count(H['infer_batch_size'])
-    correction = 0
-    while(tcount > 0):
-        tex, tey = mnist.get_batch_for_infer(H['infer_batch_size'])
-        y = creature.infer(tex)
-        tey = np.argmax(tey, -1)
-        correction += np.sum(np.equal(y, tey))
-        tcount -= 1
-    _log_bg_pp('Correction:%.2f%%' % (float(correction) / mnist.TEST_DATA_SIZE * 100))
 
 def train():
     # neuron_count, layer_count, data_in_dimention, data_out_dimention, erate
     creature = Creature(H)
-    tindex = 0
     try:
-        while (tindex < mnist.TRAIN_DATA_SIZE * 3):
-            x, y = mnist.get_batch_for_train(H['train_batch_size'])
-            creature.evolve(x, y)
-            tindex += 1
-            if (tindex % 1000 == 0):
-                time.sleep(3)
-        # train finish
-        infer(creature)
+        while (True):
+            creature.evolve(test_data, test_label)
     except KeyboardInterrupt:
         creature.__store__()
-
-if __name__ == '__main__':
-    train()
-    
