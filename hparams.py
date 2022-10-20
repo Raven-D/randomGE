ddcae4b801d21e94cfc249aa6b0611cc24ea18f3
Raven <ravend@163.com>
2022-10-20 18:37:35 +0800

    Update hparams.py

--- hparams.py
+++ hparams.py
@@ -3,7 +3,8 @@ params = {
     'layer_count':4,
     'data_in_dimention':784,
     'data_out_dimention':10,
-    'erate':0.001,
-    'cpu_sleep_step':10,
+    'erate':0.0001,
+    'cpu_sleep_step':20,
     'cpu_sleep_time':2,
+    'auto_save_step':200,
 }
