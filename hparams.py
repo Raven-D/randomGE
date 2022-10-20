abb4a1fc57d1b9e7674d56cc50b322baf6bad04c
Raven <ravend@163.com>
2022-10-20 12:28:39 +0800

    delete some unuseful params.

--- hparams.py
+++ hparams.py
@@ -1,13 +1,9 @@
 params = {
-    'neuron_count':64,
-    'layer_count':5,
+    'neuron_count':256,
+    'layer_count':4,
     'data_in_dimention':784,
     'data_out_dimention':10,
-    'erate':0.5,
-    'decay_step':1000,
-    'decay_rate':0.999,
-    'cpu_sleep_step':100,
-    'cpu_sleep_time':1.5,
-    'train_batch_size':2,
-    'infer_batch_size':1
+    'erate':0.001,
+    'cpu_sleep_step':10,
+    'cpu_sleep_time':2,
 }
